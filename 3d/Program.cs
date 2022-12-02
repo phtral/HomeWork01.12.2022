b94530c361456a8843de7092d9a5d445e5dea1cb
Philip Trubitsyn <trph@bk.ru>
2022-12-02 16:34:45 +0300

    Третья задача из домашнего задания

---
+++ 3d/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+
+if( x % 2 == 1)
+{
+    Console.WriteLine("Число является нечетным");
+    Console.WriteLine(x % 2);
+}
+else
+{
+   Console.WriteLine("Число является четным");
+   Console.WriteLine(x % 2);
+}
