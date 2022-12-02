216d310758c064fb54863e2b6bad625cc6d6a97f
Philip Trubitsyn <trph@bk.ru>
2022-12-02 14:02:49 +0300

    Первая задача из домашнего задания

---
+++ 1th/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите 1-число");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2-число");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber == secondNumber)
+{
+    Console.WriteLine("Числа являются одинаковыми. Повторите ввод!");
+}
+
+else 
+
+if ( firstNumber > secondNumber )
+{
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(secondNumber);
+}
+
+
+
+
+
+
+
