15a16d7d8302a4285306fd44a06de21a1262abc3
Philip Trubitsyn <trph@bk.ru>
2022-12-02 23:01:16 +0300

    Одна из задач на семинаре

---
+++ 5th/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input number");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number2");
+int y = int.Parse(Console.ReadLine());
+
+if(x == Math.Pow(y,2) || y == Math.Pow(y,2))
+{
+Console.WriteLine("Да, Является");
+}
+
+else{
+    Console.WriteLine("Нет, Не является ");
+}
