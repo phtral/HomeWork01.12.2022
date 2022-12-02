10e9cb853eff8283d08ca042665398b794815986
Philip Trubitsyn <trph@bk.ru>
2022-12-02 14:24:09 +0300

    Вторая задача из домашнего задания

---
+++ 2nd/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите первое число");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+if( firstNumber > secondNumber ) 
+{
+   if(firstNumber > thirdNumber)
+   {
+       Console.Write("Максимальное число ");
+       Console.WriteLine(firstNumber);
+   }
+   else{
+        Console.Write("Максимальное число ");
+        Console.WriteLine(thirdNumber);
+   }
+
+}
+ else
+ {
+    if(secondNumber > thirdNumber)
+    Console.WriteLine(secondNumber);
+    else{
+        Console.Write("Максимальное число ");
+        Console.WriteLine(thirdNumber);
+    }
+ }
+
