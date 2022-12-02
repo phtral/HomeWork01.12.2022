abdb7874371b8cf357fc607344c1f27f119d494c
Philip Trubitsyn <trph@bk.ru>
2022-12-02 16:56:22 +0300

    Четвертая задача из домашнего задания

---
+++ 4st/Program.cs
@@ -0,0 +1,10 @@
+﻿       Console.WriteLine("Задайте число N ");
+       int N = int.Parse(Console.ReadLine());
+       int chet = 2;
+
+       while (chet <= N)
+       {
+          Console.WriteLine(chet);
+          chet+=2;
+       }
+
